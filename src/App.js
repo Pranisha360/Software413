e0e7b92c918e851a4454a1d39167985db306cc1a
Jay <56737385+blackjack621@users.noreply.github.com>
2020-03-09 17:45:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,40 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const drawerWidth = 240;
 
@@ -58,15 +92,21 @@ const rows = [
 
 export default function ClippedDrawer() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className={classes.root}>
       <CssBaseline />
       <React.Fragment>
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
-            <Typography variant="h6" noWrap>
-              Clipped drawer
-            </Typography>
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+              <Tab label="Dashboard" {...a11yProps(0)} />
+              <Tab label="Reports" {...a11yProps(1)} />
+              <Tab label="Feedback" {...a11yProps(2)} />
+            </Tabs>
           </Toolbar>
         </AppBar>
         <div className={classes.offset} />
